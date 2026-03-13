0e0ecae7f64397604ece9e5da3d86bf38fcd74f3
Harshal2007vk <harshalkapale11@gmail.com>
2026-03-13 07:24:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.ensemble import RandomForestClassifier
 
 st.title("🏏 IPL Win Predictor")
+st.write("App loaded successfully")
 
 # Load dataset
 df = pd.read_csv("IPL_processed_dataset.csv")
